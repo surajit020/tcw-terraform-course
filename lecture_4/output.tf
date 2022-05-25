e5da0dcdec4bf966943716e143cc6bfa4fb64753
surajit020 <surajit020@gmail.com>
2022-05-25 13:05:57 +0530

    Delete lecture_4 directory

--- lecture_4/output.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_arn" {
-  value = aws_instance.ec2_1.arn
-}
-output "ec2_instance_state" {
-  value = aws_instance.ec2_1.instance_state
-}
-output "ec2_public_dns" {
-  value = aws_instance.ec2_1.public_dns
-}
